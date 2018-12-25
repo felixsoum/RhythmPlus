ff66b843a2b14dd768a23e8e33954dcabee6278d
Unknown <felix@felixsoum.com>
2018-12-24 22:33:35 -0500

    Add initial project

---
+++ Assets/Scripts/DiscView.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DiscView : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] SliderView sliderView;
+    RectTransform rectTransform;
+    Vector2 center;
+    Vector2 previousDragPos;
+    const float spinAngleMax = 10.0f;
+    const float spinDistanceMin = 3.0f;
+    float angle;
+    bool isDragging;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        center = RectTransformUtility.WorldToScreenPoint(null, rectTransform.position);
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        previousDragPos = eventData.position;
+        isDragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Spin(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Spin(eventData);
+        isDragging = false;
+    }
+
+    void Spin(PointerEventData eventData)
+    {
+        Vector2 currentDragPos = eventData.position;
+        float distanceToCenter = Vector2.Distance(currentDragPos, center);
+        if (distanceToCenter < spinDistanceMin)
+        {
+            previousDragPos = currentDragPos;
+            return;
+        }
+        float distance = Vector2.Distance(currentDragPos, previousDragPos);
+        Debug.Log(distanceToCenter);
+        angle = Vector2.SignedAngle(previousDragPos - center, currentDragPos - center);
+        angle = Mathf.Clamp(-spinAngleMax, angle, spinAngleMax);
+        previousDragPos = currentDragPos;
+        rectTransform.Rotate(0, 0, angle);
+        sliderView.Spin(angle);
+    }
+}
---
+++ Assets/Scripts/SliderView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SliderView : MonoBehaviour
+{
+    [SerializeField] RectTransform pointerRectTransform;
+    RectTransform rectTransform;
+    float sliderWidth;
+    float pointerWidth;
+    float leftBound;
+    float rightBound;
+    float spinSpeed;
+    float currentPointerValue = 0.5f;
+
+    void Start()
+    {
+        spinSpeed = 1 / (360.0f);
+        rectTransform = GetComponent<RectTransform>();
+        sliderWidth = rectTransform.rect.size.x;
+        pointerWidth = pointerRectTransform.rect.width;
+        leftBound = (pointerWidth - sliderWidth) / 2.0f;
+        rightBound = (sliderWidth - pointerWidth) / 2.0f;
+    }
+
+    public void Spin(float angle)
+    {
+        currentPointerValue -= angle * spinSpeed;
+        currentPointerValue = Mathf.Clamp01(currentPointerValue);
+        Vector2 anchoredPosition = new Vector2(Mathf.Lerp(leftBound, rightBound, currentPointerValue), 0);
+        pointerRectTransform.anchoredPosition = anchoredPosition;
+    }
+}
